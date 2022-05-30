ab6bfb1ef73b51eb0863a35b2f33adf1e296381e
22dam <105683935+22dam@users.noreply.github.com>
2022-05-30 21:51:13 +0900

    Add files via upload

---
+++ src/main/java/com/javateam/project/domain/QnaVO.java
@@ -0,0 +1,17 @@
+package com.javateam.project.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class QnaVO {
+
+	private int   qseq; 
+	private String subject; 
+	private String content;
+	private String reply;
+	private String id; 
+	private String rep; 
+	private Timestamp indate;
+}
